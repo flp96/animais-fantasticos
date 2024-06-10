1866ee0ebbf627a2311a6a541a2e7237ba6543e7
flp96 <filipefernandes__@live.com>
2024-06-10 16:54:39 -0300

    classes js substituidas por data atributes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
   function initTabNav() {
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');  
-    const tabContent = document.querySelectorAll('.js-tabcontent section');  
+const tabMenu = document.querySelectorAll('[data-tab="menu"] li');  
+    const tabContent = document.querySelectorAll('[data-tab="content"] section');  
 
     if(tabMenu.length && tabContent.length){
     tabContent[0].classList.add('ativo'); 
@@ -10,7 +10,8 @@ const tabMenu = document.querySelectorAll('.js-tabmenu li');
       tabContent.forEach((section) => {
         section.classList.remove('ativo');
       })
-      tabContent[index].classList.add('ativo');
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add('ativo', direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -23,7 +24,7 @@ const tabMenu = document.querySelectorAll('.js-tabmenu li');
   initTabNav();
 
   function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
     if(accordionList.length) {
     accordionList[0].classList.add(activeClass);
@@ -43,7 +44,7 @@ initAccordion();
 
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -70,23 +71,23 @@ function initScrollSuave() {
 initScrollSuave();
 
 function initAnimacaoScroll() {
-const sections = document.querySelectorAll('.js-scroll');
-if(sections.length) {
-  const windowMetade = window.innerHeight * 0.6;
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
 
-  function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else
-          section.classList.remove('ativo');
-      });
-  }
-  animaScroll();
+    function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = (sectionTop - windowMetade) < 0;
+          if(isSectionVisible)
+            section.classList.add('ativo');
+          else
+            section.classList.remove('ativo');
+        });
+    }
+    animaScroll();
 
-  window.addEventListener('scroll', animaScroll);
+    window.addEventListener('scroll', animaScroll);
+    }
   }
-}
 initAnimacaoScroll();
