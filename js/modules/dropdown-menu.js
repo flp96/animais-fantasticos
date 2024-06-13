f38027059aefe83ea3e50c05b31abcce2c334aef
flp96 <filipefernandes__@live.com>
2024-06-13 15:53:29 -0300

    funcao outsideclick adicionada

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,16 +1,21 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initDropdownMenu() {
-
-}
-
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach((menu) => {
-  ['touchstart', 'click'].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+  dropdownMenus.forEach((menu) => {
+    ['touchstart', 'click'].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    })
   })
-})
-
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle('active');
+  
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'],  () => {
+      console.log('ativou')
+        this.classList.remove('active');
+    });
+  }  
 }
+
+
