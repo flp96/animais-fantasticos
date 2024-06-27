e8461ee4092964df1c353503c7b060ddf92f73b3
flp96 <filipefernandes__@live.com>
2024-06-27 16:42:47 -0300

    animais fetch finalizado

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "/.modules/fetch-animais.js";
 
 
 initScrollSuave();
@@ -18,3 +18,4 @@ initTooltip();
 initMenuMobile();
 initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
