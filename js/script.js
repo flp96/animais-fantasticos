44c22c23e168d74e8c486e313539fb868ec57911
flp96 <filipefernandes__@live.com>
2024-07-17 20:37:37 -0300

    dropdownmenu refatorado

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
+import DropdownMenu from "./modules/dropdown-menu.js";
 
 
 
@@ -28,6 +29,9 @@ tooltip.init();
 
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
+
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
 
 
 initMenuMobile();
