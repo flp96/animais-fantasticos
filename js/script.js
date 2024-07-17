4766f2b870eca37048fa3f0576d9a14e8369bbdc
flp96 <filipefernandes__@live.com>
2024-07-16 22:14:10 -0300

    Tooltip refatorada

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
+import Tooltip from "./modules/tooltip.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
@@ -21,9 +21,13 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
+
+
 
 initAnimacaoScroll();
-initTooltip();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
