2aece43d371ac1445ab5dbc2446150ded785c44d
flp96 <filipefernandes__@live.com>
2024-07-17 11:16:48 -0300

    fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import Tooltip from "./modules/tooltip.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -30,6 +30,8 @@ tooltip.init();
 initAnimacaoScroll();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
+
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
